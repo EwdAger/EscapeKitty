a79b432d0ad00e6030962d2bc540f2347ddc605f
EwdAger <ewdager@hotmail.com>
2019-04-25 13:16:24 +0800

    基本完成爬虫部分

--- Nerds/Nerds/items.py
+++ Nerds/Nerds/items.py
@@ -10,14 +10,40 @@ from scrapy.loader import ItemLoader
 from scrapy.loader.processors import MapCompose, TakeFirst, Join
 from datetime import datetime
 from settings import SQL_DATETIME_FORMAT
+from Nerds.utils.common import get_md5
 
 
-def trans_time(value):
-    return datetime.strptime(value, '%Y-%m-%d')
+def trans_datetime(value):
+    if value:
+        return datetime.strptime(value, '%Y-%m-%d %H:%M')
+    else:
+        return value
+
+
+def trans_date(value):
+    if value:
+        return datetime.strptime(value, '%Y-%m-%d')
+    else:
+        return value
 
 
 def trans_int(value):
     return int(value)
+
+
+def edit_school(value):
+    if value:
+        return value
+    else:
+        return "湖南科技大学"
+
+
+def is_blacklist(value):
+    # 为兼容不同数据库此处不用Python中的bool类型
+    if value == "blacklist":
+        return 1
+    else:
+        return 0
 
 
 class NerdsItem(scrapy.Item):
@@ -41,10 +67,10 @@ class HnustJobModelItem(scrapy.Item):
     company_name = scrapy.Field()
     tianyan_company_url = scrapy.Field()
     publish_time = scrapy.Field(
-        input_processor=MapCompose(trans_time)
+        input_processor=MapCompose(trans_date)
     )
     end_time = scrapy.Field(
-        input_processor=MapCompose(trans_time)
+        input_processor=MapCompose(trans_date)
     )
     crawl_time = scrapy.Field()
     scale = scrapy.Field()
@@ -68,7 +94,112 @@ class HnustJobModelItem(scrapy.Item):
             self['publish_id'], self['company_name'], self['job_name'], self['end_time'],
             self['publish_time'], self['salary'], self['city_name'], self['about_major'],
             self['degree_require'], self['scale'], self['industry_category'], self['keywords'],
-            self['is_practice'], self['url'], self['tianyan_company_url'], self['crawl_time'].strftime(SQL_DATETIME_FORMAT)
+            self['is_practice'], self['url'], self['tianyan_company_url'],
+            self['crawl_time'].strftime(SQL_DATETIME_FORMAT)
         )
         return insert_sql, params
 
+
+class HnustCareersItemLoader(ItemLoader):
+    default_output_processor = TakeFirst()
+
+
+class HnustCareersItem(scrapy.Item):
+    career_talk_id = scrapy.Field()
+    url = scrapy.Field()
+    tianyan_company_url = scrapy.Field()
+    company_name = scrapy.Field()
+    professionals = scrapy.Field()
+    company_property = scrapy.Field()
+    industry_category = scrapy.Field()
+    city_name = scrapy.Field()
+    meet_name = scrapy.Field()
+    meet_time = scrapy.Field(
+        input_processor=MapCompose(trans_datetime)
+    )
+    school_name = scrapy.Field(
+        input_processor=MapCompose(edit_school)
+    )
+    address = scrapy.Field()
+    crawl_time = scrapy.Field()
+
+    def get_insert_sql(self):
+        insert_sql = """
+            INSERT INTO hnust_careers(career_talk_id, url, tianyan_company_url, company_name,
+            professionals, company_property, industry_category, city_name, meet_name, 
+            meet_time, school_name, address, crawl_time) 
+            VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)
+            ON DUPLICATE KEY UPDATE meet_time=VALUES(meet_time), address=VALUES(address)
+        """
+
+        params = (
+            self["career_talk_id"], self["url"], self["tianyan_company_url"], self["company_name"],
+            self["professionals"], self["company_property"], self["industry_category"], self["city_name"],
+            self["meet_name"], self["meet_time"], self["school_name"], self["address"],
+            self["crawl_time"].strftime(SQL_DATETIME_FORMAT)
+        )
+
+        return insert_sql, params
+
+
+class HnustJobfairsItemLoader(ItemLoader):
+    default_output_processor = TakeFirst()
+
+
+class HnustJobfairsItem(scrapy.Item):
+    fair_id = scrapy.Field()
+    url = scrapy.Field()
+    title = scrapy.Field()
+    organisers = scrapy.Field()
+    school_name = scrapy.Field()
+    address = scrapy.Field()
+    meet_time = scrapy.Field(
+        input_processor=MapCompose(trans_datetime)
+    )
+    # 参与企业数
+    plan_c_count = scrapy.Field(
+        input_processor=MapCompose(trans_int)
+    )
+    crawl_time = scrapy.Field()
+
+    def get_insert_sql(self):
+        insert_sql = """
+            INSERT INTO hnust_jobfairs(fair_id, url, title, organisers, school_name, address, meet_time, plan_c_count,
+            crawl_time)VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s)
+            ON DUPLICATE KEY UPDATE 
+            address=VALUES(address), meet_time=VALUES(meet_time), plan_c_count=VALUES(plan_c_count)
+        """
+        params = (
+            self["fair_id"], self["url"], self["title"], self["organisers"], self["school_name"], self["address"],
+            self["meet_time"], self["plan_c_count"], self["crawl_time"].strftime(SQL_DATETIME_FORMAT)
+        )
+
+        return insert_sql, params
+
+
+class ICUItemLoader(ItemLoader):
+    default_output_processor = TakeFirst()
+
+
+class ICUItem(scrapy.Item):
+    company_id = scrapy.Field(
+        input_processor=MapCompose(get_md5)
+    )
+    company_name = scrapy.Field()
+    time_desc = scrapy.Field()
+    is_blacklist = scrapy.Field(
+        input_processor=MapCompose(is_blacklist)
+    )
+    crawl_time = scrapy.Field()
+
+    def get_insert_sql(self):
+        insert_sql = """
+            INSERT INTO ICU_list(company_id, company_name, time_desc, is_blacklist, crawl_time)
+            VALUES(%s, %s, %s, %s, %s)
+            ON DUPLICATE KEY UPDATE is_blacklist=VALUES(is_blacklist), time_desc=VALUES(time_desc)
+        """
+        params = (
+            self["company_id"], self["company_name"], self["time_desc"], self["is_blacklist"], self["crawl_time"].strftime(SQL_DATETIME_FORMAT)
+        )
+
+        return insert_sql, params
--- Nerds/Nerds/spiders/hnust.py
+++ Nerds/Nerds/spiders/hnust.py
@@ -1,20 +1,36 @@
 # -*- coding: utf-8 -*-
 import scrapy
 import json
-from Nerds.items import HnustJobModelItem, HnustJobModelItemLoader
+from Nerds.items import HnustJobModelItem, HnustJobModelItemLoader, HnustCareersItem, HnustCareersItemLoader, \
+    HnustJobfairsItem, HnustJobfairsItemLoader, ICUItem, ICUItemLoader
 from datetime import datetime
+from scrapy.xlib.pydispatch import dispatcher
+from scrapy import signals
 
 
 class HnustSpider(scrapy.Spider):
+    def __init__(self):
+        pass
+
     name = 'hnust'
     allowed_domains = ['jy.hnust.edu.cn']
     start_urls = ['http://jy.hnust.edu.cn/']
 
+
     def start_requests(self):
         # 爬取前两万条岗位信息
         for i in range(2):
-            yield scrapy.Request("http://jy.hnust.edu.cn/module/getjobs?start_page=1&type_id=-1&k=&is_practice={0}&count=5&start=1".format(i),
+            yield scrapy.Request("http://jy.hnust.edu.cn/module/getjobs?start_page=1&type_id=-1&k=&is_practice={0}&count=20000&start=1".format(i),
                                  callback=self.handle_jobs)
+        # 爬取宣讲会信息
+        for i in ["inner", "outer"]:
+            yield scrapy.Request("http://jy.hnust.edu.cn/module/getcareers?start_page=1&k=&type={0}&day=&count=10000&start=1".format(i),
+                                 callback=self.handle_careers)
+        # 爬取双选会信息
+        yield scrapy.Request("http://jy.hnust.edu.cn/module/getjobfairs?start_page=1&keyword=&count=300&start=1", callback=self.handle_jobfairs)
+        for i in ["blacklist", "whitelist"]:
+            yield scrapy.Request("https://github.com/996icu/996.ICU/tree/master/{0}".format(i), meta={"type": i},
+                                 callback=self.get_996ICU)
 
     def handle_jobs(self, response):
         jobs = json.loads(response.body_as_unicode())["data"]
@@ -39,3 +55,52 @@ class HnustSpider(scrapy.Spider):
             job_item = item_loader.load_item()
             yield job_item
 
+    def handle_careers(self, response):
+        careers = json.loads(response.body_as_unicode())["data"]
+        for career in careers:
+            item_loader = HnustCareersItemLoader(item=HnustCareersItem())
+            item_loader.add_value("career_talk_id", career["career_talk_id"])
+            item_loader.add_value("url", "http://jy.hnust.edu.cn/detail/career?id="+career["career_talk_id"])
+            item_loader.add_value("tianyan_company_url", "https://www.tianyancha.com/search?key="+career["company_name"])
+            item_loader.add_value("company_name", career["company_name"])
+            item_loader.add_value("professionals", career["professionals"])
+            item_loader.add_value("company_property", career["company_property"])
+            item_loader.add_value("industry_category", career["industry_category"])
+            item_loader.add_value("city_name", career["city_name"])
+            item_loader.add_value("meet_name", career["meet_name"])
+            item_loader.add_value("meet_time", " ".join([career["meet_day"], career["meet_time"]]))
+            item_loader.add_value("school_name", career["school_name"])
+            item_loader.add_value("address", career["address"])
+            item_loader.add_value("crawl_time", datetime.now())
+            career_loader = item_loader.load_item()
+            yield career_loader
+
+    def handle_jobfairs(self, response):
+        jobfairs = json.loads(response.body_as_unicode())["data"]
+        for jobfair in jobfairs:
+            item_loader = HnustJobfairsItemLoader(item=HnustJobfairsItem())
+            item_loader.add_value("fair_id", jobfair["fair_id"])
+            item_loader.add_value("url", "http://jy.hnust.edu.cn/detail/jobfair?id="+jobfair["fair_id"])
+            item_loader.add_value("title", jobfair["title"])
+            item_loader.add_value("organisers", jobfair["organisers"])
+            item_loader.add_value("school_name", jobfair["school_name"])
+            item_loader.add_value("address", jobfair["address"])
+            item_loader.add_value("meet_time", " ".join([jobfair["meet_day"], jobfair["meet_time"]]))
+            item_loader.add_value("plan_c_count", jobfair["plan_c_count"])
+            item_loader.add_value("crawl_time", datetime.now())
+            jobfair_loader = item_loader.load_item()
+            yield jobfair_loader
+
+    def get_996ICU(self, response):
+        list_type = response.meta.get("type")
+        table_num = 2 if list_type == "blacklist" else 1
+        count_tr = len(response.xpath("//*[@id='readme']/div[2]/article/table[{0}]/tbody/tr".format(table_num)))
+        for i in range(1, count_tr+1):
+            item_loader = ICUItemLoader(item=ICUItem(), response=response)
+            item_loader.add_xpath("company_id", "//*[@id='readme']/div[2]/article/table[{0}]/tbody/tr[{1}]/td[2]/a/text()".format(table_num, i))
+            item_loader.add_xpath("company_name", "//*[@id='readme']/div[2]/article/table[{0}]/tbody/tr[{1}]/td[2]/a/text()".format(table_num, i))
+            item_loader.add_xpath("time_desc", "//*[@id='readme']/div[2]/article/table[{0}]/tbody/tr[{1}]/td[4]/text()".format(table_num, i))
+            item_loader.add_value("is_blacklist", list_type)
+            item_loader.add_value("crawl_time", datetime.now())
+            ICU_loader = item_loader.load_item()
+            yield ICU_loader
