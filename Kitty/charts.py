c48604e29804cb83e9b57b32a1bd87086a8ebed5
EwdAger <ewdager@hotmail.com>
2019-05-20 10:31:20 +0800

    添加计算机相关信息图表

--- Kitty/charts.py
+++ Kitty/charts.py
@@ -2,6 +2,7 @@ import MySQLdb
 import MySQLdb.cursors
 from pyecharts import Bar, Pie, Line, WordCloud
 from django_echarts.views.backend import EChartsBackendView
+import collections
 
 db = MySQLdb.connect("localhost", "root", "33Miss77###", "hnjy", charset='utf8')
 
@@ -68,7 +69,7 @@ def get_timelist():
     except:
         pass
     cursor.close()
-    return timelist, nums
+    return timelist[::-1], nums[::-1]
 
 
 def get_wordcloud():
@@ -93,7 +94,65 @@ def get_wordcloud():
     except:
         pass
     cursor.close()
-    return major[::-1], nums[::-1]
+    return major, nums
+
+
+def do_salary(name):
+    sql = """
+        SELECT job_name,  salary ,  COUNT(*)
+        FROM hnust_jobs 
+        GROUP BY job_name,  salary
+        HAVING job_name LIKE "%{0}%"
+    """.format(name)
+    data = collections.OrderedDict()
+    cursor = db.cursor()
+    try:
+        cursor.execute(sql)
+        result = cursor.fetchall()
+        for i in result:
+            temp = i[1]
+            temp = temp.replace("/月", "").replace("K", "")
+            temp = temp.split("-")
+            temp = [int(float(i)) for i in temp]
+            for j in range(2):
+                if temp[j] > 50:
+                    continue
+                if str(temp[j]) in data:
+                    data[str(temp[j])] += i[2]
+                else:
+                    data[str(temp[j])] = i[2]
+    except:
+        pass
+    cursor.close()
+    data = sorted(data.items(), key=lambda item: int(item[0]))
+    salary = [str(k)+"K/月" for k, v in data]
+    nums = [v for k, v in data]
+
+    return salary, nums
+
+
+def do_nums(name):
+    nums = 0
+    sql = """
+        SELECT job_name, COUNT(*)
+        FROM hnust_jobs 
+        GROUP BY job_name
+        HAVING job_name LIKE "%{0}%"
+    """.format(name)
+    data = collections.OrderedDict()
+    cursor = db.cursor()
+    try:
+        cursor.execute(sql)
+        result = cursor.fetchall()
+        for i in result:
+            nums += int(i[1])
+    except:
+        pass
+    cursor.close()
+
+    return nums
+
+
 
 
 def charts(name):
@@ -120,3 +179,24 @@ def charts(name):
         major, nums = get_wordcloud()
         word.add("数量", major, nums, shape="diamond")
         return word
+
+    elif name == "com":
+        com = Line("", "")
+        data = []
+        for name in ["python", "php", "java", "c++", "前端", "后端", "安卓"]:
+            salary, nums = do_salary(name)
+            data.append(salary)
+            com.add(name, salary, nums)
+        return com
+
+    elif name == "jobs":
+        jobs = Bar("", "")
+        names = []
+        nums = []
+        for name in ["python", "php", "java", "c++", "前端", "后端", "安卓"]:
+            num = do_nums(name)
+            names.append(name)
+            nums.append(num)
+        jobs.add("数量", names, nums)
+
+        return jobs
